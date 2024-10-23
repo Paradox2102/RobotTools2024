ca1797b4bb52204f25601ac16f8f48ec83c9e291
Picky Eats <69869896+PickyEats@users.noreply.github.com>
2024-10-22 17:16:16 -0700

    MarlowsRobot

---
+++ VSCode/MarlowsRobot/src/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robotCore.Device;
+import robotCore.Logger;
+import robotCore.PWMMotor;
+
+public class DriveSubsystem extends SubsystemBase {
+  private static final int k_leftMotorPWMPin = Device.M1_2_PWM;
+  private static final int k_leftMotorDirPin = Device.M1_2_DIR;
+  private static final int k_rightMotorPWMPin = Device.M1_1_PWM;
+  private static final int k_rightMotorDirPin = Device.M1_1_DIR;
+  
+  private PWMMotor m_leftmotor = new PWMMotor(k_leftMotorPWMPin, k_leftMotorDirPin);
+  private PWMMotor m_rightmotor = new PWMMotor(k_rightMotorPWMPin, k_rightMotorDirPin);
+  /**
+   * Creates a new DriveSubsystem.
+   */
+  public DriveSubsystem() {
+    Logger.log("DriveSubsystem", 3, "DriveSubsystem()");
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    Logger.log("DriveSubsystem", -1, "periodic()");
+
+  }
+
+  public void setpower(double leftpower, double rightpower){
+   
+    m_leftmotor.set(leftpower);
+    m_rightmotor.set(rightpower);
+
+  }
+}
